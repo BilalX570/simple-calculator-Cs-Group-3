d9d46222e780c8abc65d6381041fe1913fe725cc
Bilal Muktar <bilalmuktar192@gmail.com>
2025-04-05 19:33:57 -0300

    Implemented addition functionality

---
+++ simple calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+double addition(double a, double b) {
+    return a+b;
+}
+int main() {
+
+cout<<"Result:"<<addition(first_number,second_number)<<endl;
+
+
+   
+return 0;
+}
