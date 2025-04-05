d892874804760c3123eb5555897d3715d900032f
Eyerus <eyeruswesene378@gmail.com>
2025-04-06 01:24:50 +0300

    implemented substraction functionality

---
+++ simple calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+double substraction(double a, double b) {
+    return a-b;
+}
+int main() {
+
+    cout<<"Result:"<<substraction(first_number,second_number)<<endl;
+    break;
+
+    
+   
+return 0;
+}
