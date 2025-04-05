82ff7e5d2fb7f956de03e28964109436472ccc27
Bilal Muktar <bilalmuktar192@gmail.com>
2025-04-05 19:53:58 -0300

    Implement the  code that ask the user to enter the numbers

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -5,6 +5,10 @@ double addition(double a, double b) {
     return a+b;
 }
 int main() {
+    cout<<"Enter the first number: ";
+    cin>>first_number;
+    cout<<"Enter the second number: ";
+    cin>>second_number;
 
 cout<<"Result:"<<addition(first_number,second_number)<<endl;
 
