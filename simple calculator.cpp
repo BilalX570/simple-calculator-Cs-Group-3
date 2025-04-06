be657ef0e567de55365070ec51c48762fc4b336e
Bilal Muktar <bilalmuktar192@gmail.com>
2025-04-06 04:31:40 -0400

    implement addition functionality

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+double addition(double a,double b) {
+    return a+b;
+}
 double power (double a, double b) {
     return pow(a,b);
 }
 int main() {
-cout<< "result:"<<power (first_number,second_number)<<endl;
+cout<<"result:"<<addition(firs_number,second_number)<<endl;
+cout<<"result:"<<power (first_number,second_number)<<endl;
 return 0;
 }
