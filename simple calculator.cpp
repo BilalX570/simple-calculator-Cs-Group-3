8a1809f23a58e7b78d4cb58f34afe869965ee1af
haftam <haftambirhanu164@gmail.com>
2025-04-06 01:15:21 -0700

    implemented multiplication functionality

---
+++ simple calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+ double multiplication(double a, double b) {
+    return a*b;
+}
+int main() {
+    cout<<"Result:"<<multiplication(first_number,second_number)<<endl;
+
+
+    
+   
+return 0;
+}
